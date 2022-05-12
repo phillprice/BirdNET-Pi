ca6d70dbde9025fcb1eee18123eae890178604fc
ehpersonal38 <103586016+ehpersonal38@users.noreply.github.com>
2022-05-12 18:34:05 -0400

    CC images with proper attribution!
    ditching public domain images, some of them are less than ideal

--- scripts/stats.php
+++ scripts/stats.php
@@ -107,12 +107,52 @@ $filename = "/By_Date/".$results['Date']."/".$comname."/".$results['File_Name'];
 </table>
 </div>
 <div class="column center">
+
 <?php if(!isset($_GET['species'])){
 ?><p class="centered">Choose a species to load images from Flickr.</p>
 <?php
 };?>
 <?php if(isset($_GET['species'])){
+
+?> 
+<style>
+dialog::backdrop {
+  background: repeating-linear-gradient(
+    30deg,
+    rgba(24, 194, 236, 0.2),
+    rgba(24, 194, 236, 0.2) 1px,
+    rgba(24, 194, 236, 0.3) 1px,
+    rgba(24, 194, 236, 0.3) 20px
+  );
+  backdrop-filter: blur(1px)
+}
+</style>
+<dialog id="attribution-dialog">
+  <h1 id="modalHeading"></h1>
+  <p id="modalText"> I am a dialog-box with backdrop</p>
+  <button onclick="hideDialog()">Close</button>
+</dialog>
+<script>
+var dialog = document.querySelector('dialog');
+dialogPolyfill.registerDialog(dialog);
+
+function showDialog() {
+  document.getElementById('attribution-dialog').showModal();
+}
+
+function hideDialog() {
+  document.getElementById('attribution-dialog').close();
+}
+
+function setModalText(iter, title, text, authorlink) {
+  document.getElementById('modalHeading').innerHTML = "Photo "+iter+": \""+title+"\" Attribution";
+  document.getElementById('modalText').innerHTML = "Image link: <a target='_blank' href="+text+">"+text+"</a><br>Author link: <a target='_blank' href="+authorlink+">"+authorlink+"</a>";
+  showDialog();
+}
+</script>    
+<?php
   $species = $_GET['species'];
+  $iter=0;
    
 while($results=$result3->fetchArray(SQLITE3_ASSOC)){
   $count = $results['COUNT(*)'];
@@ -142,13 +182,17 @@ while($results=$result3->fetchArray(SQLITE3_ASSOC)){
   
   ob_flush();
   flush();
-  $flickrjson = json_decode(file_get_contents("https://www.flickr.com/services/rest/?method=flickr.photos.search&api_key=2158f6c5d66e89778bd0d340953f1bcf&text=".str_replace('_', '+', $comname)."&license=7%2C9%2C10&sort=relevance&per_page=15&format=json&nojsoncallback=1"), true)["photos"]["photo"];
+  $flickrjson = json_decode(file_get_contents("https://www.flickr.com/services/rest/?method=flickr.photos.search&api_key=2158f6c5d66e89778bd0d340953f1bcf&text=".str_replace('_', '+', $comname)."&license=2%2C3%2C4%2C5%2C6%2C9&sort=relevance&per_page=15&format=json&nojsoncallback=1"), true)["photos"]["photo"];
   foreach ($flickrjson as $val) {
+      $iter++;
+      $modaltext = "https://flickr.com/photos/".$val["owner"]."/".$val["id"];
+      $authorlink = "https://flickr.com/people/".$val["owner"];
       $imageurl = 'http://farm' .$val["farm"]. '.static.flickr.com/' .$val["server"]. '/' .$val["id"]. '_'  .$val["secret"].  '.jpg';
-      echo "<img style='vertical-align:top' src=\"$imageurl\">";
+      echo "<img style='vertical-align:top' src=\"$imageurl\"><span style='cursor:pointer;color:blue;text-decoration:underline;' onclick='setModalText(".$iter.",\"".$val["title"]."\",\"".$modaltext."\", \"".$authorlink."\")'>".$iter."</span>";
   }
 }}
 ?>
+
 <br><br><br>
 
     <table>
